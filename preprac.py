0efb89754edcf0cb92eb7f9cf930c25f499e1de0
lindelanimcebo <lindelanimcebo@gmail.com>
2020-09-24 17:11:11 +0100

    Toggle LED with switch

--- preprac.py
+++ preprac.py
@@ -10,22 +10,24 @@ SWITCH = 18
 GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED,GPIO.OUT)
 
+def toggle(pin):
+	GPIO.output(LED, not GPIO.input(LED))
+
 def main():
-	time.sleep(0.5)
-	GPIO.output( LED, not GPIO.input(LED) )
+	GPIO.add_event_detect(SWITCH, GPIO.FALLING, callback=toggle, bouncetime = 200)
+	while True:
+		time.sleep(0.01)
 
 if __name__ == "__main__":
 	# Making sure that GPIO stopped correctly
 	try:
-		while True:
-			main()
+		main()
 	except KeyboardInterrupt:
 		print("Exiting Nicely!")
-		GPIO.cleanup()
-	except e:
+	except Exception as e:
 		print("Some other error occured: {}".format(e.message))
+	finally:
 		GPIO.cleanup()
-
 
 
 
